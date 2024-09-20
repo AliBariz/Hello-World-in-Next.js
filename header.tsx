7266dd3af03378dd640d3be5b3b0bf1b3fdbb62c
AliBariz <160329277+AliBariz@users.noreply.github.com>
2024-09-20 20:28:28 +0500

    Add files via upload

---
+++ header.tsx
@@ -0,0 +1,17 @@
+function Header(){
+    return(
+        <div>  
+        <h1>this is header</h1>
+        </div>
+    )
+}
+
+export default Header
+
+// function Footer(){
+//     return(
+//         <h2>this is footer</h2>
+//     )
+// }
+
+// export default Footer
---
+++ page.tsx
@@ -0,0 +1,10 @@
+import Header from "./components/header";
+
+export default function Home() {
+  return (
+    <div>
+    <h1>Hello World!</h1>
+    <Header/>
+    </div>
+  );
+}
